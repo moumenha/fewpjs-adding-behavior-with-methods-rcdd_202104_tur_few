b742d22d9a8c39fbfbcfc74eb5601105a5b9f21e
Moumen Hallak <moumen95@gmail.com>
2021-05-19 04:47:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+class Cat{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+}
+
+class Dog{
+  constructor(name, sex){
+    
+  }
+
+}
+
+class Bird{
+  constructor(name, sex){
+    
+  }
+
+}
